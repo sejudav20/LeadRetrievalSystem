ab2c17c0a78e90c8d6c20405100411334507bf5c
Davin <davsej100@gmail.com>
2020-01-17 14:18:46 -0800

    ffyfyf

--- app/src/main/java/com/example/conferencepro/MainActivity.java
+++ app/src/main/java/com/example/conferencepro/MainActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    final String user=getSharedPreferences("user",MODE_PRIVATE).getString("username","guest");;
+     String user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        user=getSharedPreferences("user",MODE_PRIVATE).getString("username","guest");
+        //TODO Please add initial case to see if user has been here before
 
     }
 }
