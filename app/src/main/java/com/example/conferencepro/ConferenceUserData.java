c248f92205d0df47ccfecfa7e5a30d3eff8d5e2f
Davin <davsej100@gmail.com>
2020-02-03 14:00:29 -0800

    Added

--- app/src/main/java/com/example/conferencepro/ConferenceUserData.java
+++ app/src/main/java/com/example/conferencepro/ConferenceUserData.java
@@ -1,18 +1,31 @@
 package com.example.conferencepro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class ConferenceUserData extends AppCompatActivity {
-    ArrayList<String> companies;
+    HashMap<String, Boolean> companies;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conference_user_data);
-        companies=new ArrayList<>();
+        companies=new HashMap<>();
+        Scanner sc= new Scanner(getSharedPreferences("ConferenceData",MODE_PRIVATE).getString("cData",""));
+        sc.useDelimiter(",");
+        String s=sc.next();
+        while(sc.hasNext()){
+            companies.put(sc.next(),false);
+
+        }
+        recyclerView= findViewById(R.id.);
 
     }
 }
--- app/src/main/java/com/example/conferencepro/EntrantMain.java
+++ app/src/main/java/com/example/conferencepro/EntrantMain.java
@@ -1,8 +1,11 @@
 package com.example.conferencepro;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.Strategy;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -13,14 +16,20 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class EntrantMain extends AppCompatActivity {
     TextView tx;
     String conference = "";
     Button b;
     Button toUserData;
+    EditText confNumber;
+    TextView txe;
     public static String user;
 
     public static String getUser() {
@@ -28,23 +37,100 @@ public class EntrantMain extends AppCompatActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrant_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        SharedPreferences sp= getSharedPreferences("ConferenceData",MODE_PRIVATE);
         b = findViewById(R.id.AddConference);
+        txe=findViewById(R.id.textView3);
         b.setVisibility(View.INVISIBLE);
-        if (conference.equals("")) {
-            b.setVisibility(View.VISIBLE);
+        if (sp.getString("cName","").equals("")) {
 
+            b.setVisibility(View.VISIBLE);
+            confNumber.setVisibility(View.VISIBLE);
+            txe.setText("No conference Joined:");
+        }else{
+            txe.setText("Current Conference is:"+sp.getString("cName",""));
         }
+        confNumber= findViewById(R.id.editText);
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO  add connection to nearby conference beacon
+
+                NearbyCreator nc= new NearbyCreator(EntrantMain.this,"Usr", Strategy.P2P_CLUSTER);
+                nc.startDiscovery(confNumber.getText().toString()+" "+new Random().nextInt(10000000),new NearbyCreator.OptionsOfDiscovery(){
+                    @Override
+                    public void OnDiscoverySuccess() {
+                        Toast.makeText(EntrantMain.this,"Discovery of conferences starting",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void OnDiscoveryFailure() {
+                        Toast.makeText(EntrantMain.this,"Discovery of conferences Failed",Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void OnStringReceived(String s) {
+                        String confname=new Scanner(s).next();
+                        Toast.makeText(EntrantMain.this,"Connection successful to conference"+ confname,Toast.LENGTH_SHORT).show();
+                        SharedPreferences sp= getSharedPreferences("ConferenceData",MODE_PRIVATE);
+                        sp.edit().putString("CData",s).apply();
+                        sp.edit().putString("cName",confname).apply();
+                        b.setVisibility(View.INVISIBLE);
+                        confNumber.setVisibility(View.INVISIBLE);
+                        txe.setText("Current Conference is:"+sp.getString("cName",""));
+                    }
+
+                    @Override
+                    public void OnStringUpdate() {
+
+                    }
+
+                    @Override
+                    public void OnConnectionGood(String s) {
+
+                    }
+
+                    @Override
+                    public void OnConnectionError() {
+
+                    }
+
+                    @Override
+                    public void OnConnectionRejected() {
+
+                    }
+
+                    @Override
+                    public void OnConnectionDisconnected() {
+
+                    }
+
+                    @Override
+                    public boolean Authenticated(@NonNull DiscoveredEndpointInfo discoveredEndpointInfo) {
+                        if(discoveredEndpointInfo.getEndpointName().equals(confNumber.getText().toString())){
+                        return true;}else{return false;}
+                    }
+
+                    @Override
+                    public void OnConnectionSuccess() {
+
+                    }
+
+                    @Override
+                    public void OnConnectionFailure() {
+                        Toast.makeText(EntrantMain.this,"Connection did not work",Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void OnConnectionLost() {
+
+                    }
+                });
+
 
             }
         });
