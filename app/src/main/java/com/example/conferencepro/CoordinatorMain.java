09bcb817f7fd1cbb3ed8019c0e2b1900e8a5511b
Davin <davsej100@gmail.com>
2020-02-06 14:17:14 -0800

    Context

--- app/src/main/java/com/example/conferencepro/CoordinatorMain.java
+++ app/src/main/java/com/example/conferencepro/CoordinatorMain.java
@@ -7,21 +7,38 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.nearby.connection.Strategy;
 
-public class CoordinatorMain extends AppCompatActivity {
+public class CoordinatorMain extends AppCompatActivity implements RoleTransferDialogBox.RoleTransferCallback {
     String conference;
     SharedPreferences sp;
     NearbyCreator nc;
     String name;
+    boolean isReceiving;
+    int conferenceNumber;
+    TextView welcome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinator_main);
+
+
         sp=getSharedPreferences("CoordData",MODE_PRIVATE);
+        SharedPreferences spe=getSharedPreferences("user",MODE_PRIVATE);
+        welcome.setText("Welcome "+spe.getString("username",""));
+
         conference=sp.getString("cName","");
+        conferenceNumber=sp.getInt("cNum",0);
+        if(conferenceNumber==0){
+
+
+
+        }
+
         name="";
         nc= new NearbyCreator(this,name, Strategy.P2P_CLUSTER);
     }
@@ -31,14 +48,18 @@ public class CoordinatorMain extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.TransferCoordinatorRole:
                 if (!conference.equals("")) {
-                    Toast.makeText(CoordinatorMain.this,"Please be a few feet whithin the new phone with this conference role", Toast.LENGTH_LONG).show();
+                    Toast.makeText(CoordinatorMain.this,"Please be a few feet within the new phone with this conference role", Toast.LENGTH_LONG).show();
+                    isReceiving=false;
+
                 } else {
                     Toast.makeText(CoordinatorMain.this,"Please add a conference first", Toast.LENGTH_LONG).show();
+
                 }
 
                 break;
             case R.id.ReceiveCoordinatorRole:
                 Toast.makeText(CoordinatorMain.this,"Please add a conference first", Toast.LENGTH_LONG).show();
+                isReceiving=true;
                 break;
 
             default:
@@ -50,4 +71,19 @@ public class CoordinatorMain extends AppCompatActivity {
 
     }
 
+    @Override
+    public void whenPositiveButtonClicked() {
+        if(isReceiving){
+            nc.startDiscovery("Conference receiver" ,);
+
+        }else{
+            nc.startAdvertising("");
+
+        }
+    }
+
+    @Override
+    public void whenNegativeButtonClicked() {
+
+    }
 }
--- app/src/main/java/com/example/conferencepro/RoleTransferDialogBox.java
+++ app/src/main/java/com/example/conferencepro/RoleTransferDialogBox.java
@@ -2,6 +2,7 @@ package com.example.conferencepro;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
@@ -9,10 +10,23 @@ import androidx.fragment.app.DialogFragment;
 
 public class RoleTransferDialogBox extends DialogFragment {
     public interface RoleTransferCallback{
-        
+         void whenPositiveButtonClicked();
+        void whenNegativeButtonClicked();
 
 
     }
+     RoleTransferCallback listener;
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        listener=(RoleTransferCallback) context;
+
+
+
+
+
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
@@ -21,12 +35,12 @@ public class RoleTransferDialogBox extends DialogFragment {
             builder.setMessage("Please be close enough to the new phone. Advertising will end after a minute or after connection is successful")
                     .setPositiveButton("Continue", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
-
+                            listener.whenPositiveButtonClicked();
                         }
                     })
                     .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
-
+                            listener.whenNegativeButtonClicked();
                         }
                     });
 
